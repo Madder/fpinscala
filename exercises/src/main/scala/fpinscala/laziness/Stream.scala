1cc3a65d4606a40855e835939f4df1a0046fea64
REDDAM Omar <Omar.REDDAM@murex.com>
2016-02-29 20:14:19 +0100

    wip, ch5, lazyness

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -3,6 +3,7 @@ package fpinscala.laziness
 import Stream._
 trait Stream[+A] {
 
+
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -17,15 +18,85 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+  //Mine
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(hd, tl) => hd()::(tl().toList)
+  }
+  //From answers
+  def toList_2: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  /*
+  In order to avoid the `reverse` at the end, we could write it using a
+  mutable list buffer and an explicit loop instead. Note that the mutable
+  list buffer never escapes our `toList` method, so this function is
+  still _pure_.
+  */
+  def toListFast: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h,t) =>
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  // 1st impl (not stack safe)
+  def take(n: Int): Stream[A] = {
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+    def go(s: => Stream[A], m: Int) : Stream[A] = if (m<=0) s else s match {
+      case Cons(h,t) => cons(h(), go(t(), m-1))
+      case Empty => empty
+    }
 
-  def headOption: Option[A] = sys.error("todo")
+    go(this, n)
+
+  }
+
+  /*
+  Create a new Stream[A] from taking the n first elements from this. We can achieve that by recursively
+  calling take on the invoked tail of a cons cell. We make sure that the tail is not invoked unless
+  we need to, by handling the special case where n == 1 separately. If n == 0, we can avoid looking
+  at the stream at all.
+*/
+  def take_fpinscala_1(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take_fpinscala_1(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h,t) if n>0 => t().drop(n-1)
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h,t) if p(h())==true => cons(h(), t().takeWhile(p))
+    case _ => empty
+  }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h,t) =>  p(h()) && t().forAll(p)
+    case Empty => true
+  }
+
+  def forAllViaFoldRight(f: A => Boolean): Boolean =
+    foldRight(true)((a,b) => f(a) && b)
+
+  def headOption: Option[A] = this match {
+    case Cons(h,t) => Some(h())
+    case Empty => None
+  }
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
